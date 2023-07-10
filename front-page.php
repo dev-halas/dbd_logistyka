727f37fde515882869f0397b9aa7020e5d295b3e
kamilogrodowczyk <kamil.web.development@gmail.com>
2023-07-10 20:26:22 +0200

    Add dbd sections

--- front-page.php
+++ front-page.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
-GŁÓWNA
+    <main>
+        <?php 
+            get_template_part('/components/home/offer');
+            get_template_part('/components/home/cars');
+            get_template_part('/components/home/numbers');
+        ?>
+    </main>
 
 <a href="#" class="button">BUTTON</a>
 
