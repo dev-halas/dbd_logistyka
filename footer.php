be99b5c9489957bec5fda2ee2e1eca6a8c0a3b62
Krystia Hałas <dev.halas@gmail.com>
2023-07-12 23:15:53 +0200

    add footer

--- footer.php
+++ footer.php
@@ -1,6 +1,70 @@
-	<footer>
+	<footer class="footer">
 		<?php wp_footer(); ?>
-		<h1>Footer</h1>
+		<div class="footerInner container">
+			<div class="footerInner__full">
+				<img src="<?php echo THEME_URL; ?>_dev/img/logo.svg" alt="DBD logo" class="footerLogo">
+				<span></span>
+			</div>
+			<div class="footerInner__col">
+				<div class="footer__comapnyInfo">
+					<h3>DBD Trans</h3>
+					<div>
+						Aleja Powstańców Wlkp. 30 /3<br/>
+						63-700 Krotoszyn, Polska<br/>
+						NIP: 1234567890<br/>
+						Regon: 123456789<br/>
+						KRS: 1234567890
+					</div>
+				</div>
+			</div>
+			<div class="footerInner__col">
+				<div class="footer__contactPersons">
+					<div class="footer__contactPerson">
+						<div class="footerFlags">
+							<img src="<?php echo THEME_URL; ?>_dev/img/poland.svg" alt="">
+							<img src="<?php echo THEME_URL; ?>_dev/img/germany.svg" alt="">
+							<img src="<?php echo THEME_URL; ?>_dev/img/uk.svg" alt="">
+						</div>
+						<h3 class="gradientText">Marek</h3>
+						<div>
+							<a href="tel: +48 517 706 357">+48 517 706 357</a>
+							<a href="mailto: dispo@dbdtrans.pl">dispo@dbdtrans.pl</a>
+						</div>
+					</div>
+					<div class="footer__contactPerson">
+						<div class="footerFlags">
+							<img src="<?php echo THEME_URL; ?>_dev/img/poland.svg" alt="">
+							<img src="<?php echo THEME_URL; ?>_dev/img/germany.svg" alt="">
+						</div>
+						<h3 class="gradientText">Marek</h3>
+						<div>
+							<a href="tel: +48 517 706 357">+48 517 706 357</a>
+							<a href="mailto: dispo@dbdtrans.pl">dispo@dbdtrans.pl</a>
+						</div>
+					</div>
+					<div class="footer__contactPerson">
+						<div class="footerFlags">
+							<img src="<?php echo THEME_URL; ?>_dev/img/poland.svg" alt="">
+							<img src="<?php echo THEME_URL; ?>_dev/img/uk.svg" alt="">
+						</div>
+						<h3 class="gradientText">Marek</h3>
+						<div>
+							<a href="tel: +48 517 706 357">+48 517 706 357</a>
+							<a href="mailto: dispo@dbdtrans.pl">dispo@dbdtrans.pl</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footerInner__copy">
+				<div class="footerCopy">
+					<span>© 2023 dbdtrans.pl</span>
+					<a href="#">Polityka prywatności oraz cookies</a>
+				</div>
+				<div>
+					Strona wdrożona z pasją przez <strong>Sharing Design. Studio Graficzne</strong>
+				</div>
+			</div>
+		</div>
 	</footer>
 </body>
 
